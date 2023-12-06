282d1b58df8c69272d08f5b7ddaf233ac426e4b3
SevinaMar <149876042+SevinaMar@users.noreply.github.com>
2023-12-06 13:23:39 +0200

    Add files via upload

---
+++ ergasia_ari8mos-pow.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+	int ari8mos, square, time;
+		time=1;
+		printf("Select integer %d:\n",time);
+		scanf("%d", &ari8mos);	
+	while (ari8mos>0){
+		 square=pow(ari8mos,2);
+		printf("%d squared is %d\n", ari8mos, square);
+		printf("\nSelect an integer %d:\n", time);
+		scanf("%d", &ari8mos);
+		time++;
+		
+		
+	} 
+	if (ari8mos<0){
+		printf("%d is a negative number.\n", ari8mos);
+	}
+	else {
+		printf("You chose 0.\n");
+	}
+	
+	printf ("\nYou selected %d numbers.", time);
+	return 0;
+}
